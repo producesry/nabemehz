bacc74485828a37f20b2f30719bf913db1548c9c
LiBook <libook7@qq.com>
2016-09-16 14:19:30 +0800

    Finish POST /live/:liveId/like .

--- routes/clientRoutes/liveRoutes.js
+++ routes/clientRoutes/liveRoutes.js
@@ -37,6 +37,40 @@ router.get('live', function *() {
 });
 
 /**
+ * 用户点击想看
+ * POST /live/:liveId/like
+ * Request:
+ *      Param:
+ *          liveId 直播的_id，通常为GET /live返回Body的next._id
+ *      Header:
+ *          Authorization: Token //客户端本地记录的Token
+ * Response:
+ *      Body:
+ *          {Live Object}
+ */
+router.post('live/:liveId/like', tokenMidw.verify(), function *() {
+    let existsWish = yield MODEL.Wishlist.findOne({
+        "userId": this.state.user.id,
+        "wishType": "live",
+        "wishId": this.params.liveId
+    });
+    let live = yield MODEL.Live.findOne().populate('next.doctor', 'preview.video');
+    if (this.params.liveId == live.next._id.toString()) {
+        if (existsWish == null) {
+            let wishlist = new MODEL.Wishlist({
+                "userId": this.state.user.id,
+                "wishType": "live",
+                "wishId": this.params.liveId
+            });
+            yield wishlist.save();
+            live.next.like += 1;
+            yield live.save();
+        }
+    }
+    this.body = live;
+});
+
+/**
  * Define routes end.
  */
 
