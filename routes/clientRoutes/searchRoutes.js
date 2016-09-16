776ccdcc8bb03cc784ac50bc1ec0aa36575603d3
LiBook <libook7@qq.com>
2016-09-16 13:26:26 +0800

    Finish POST /search .

--- routes/clientRoutes/searchRoutes.js
+++ routes/clientRoutes/searchRoutes.js
@@ -24,7 +24,34 @@ const router = new Router();
  * Define routes start.
  */
 
-
+/**
+ * 搜索内容
+ * POST /search
+ * Request:
+ *      Query String:
+ *          word = String //搜索的关键词，目前只支持单个关键词
+ * Response:
+ *      Body:
+ *          {
+ *              "doctors": [
+ *                  {Doctor Object}
+ *              ],
+ *              "videos": [
+ *                  {Video Object}
+ *              ]
+ *          }
+ */
+router.post('search', function *() {
+    this.body = {
+        "doctors": yield MODEL.Doctor.find({
+            "$or": [
+                {"name": this.request.query.word},
+                {"speciality": this.request.query.word}
+            ]
+        }),
+        "videos": yield MODEL.Video.find({"label": this.request.query.word})
+    };
+});
 
 /**
  * Define routes end.
