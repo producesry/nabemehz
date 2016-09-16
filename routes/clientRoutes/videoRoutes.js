5caee1bfcdd2086cb2a512a1528e140c986a676e
LiBook <libook7@qq.com>
2016-09-16 12:54:48 +0800

    Finish GET /video/:videoId .

--- routes/clientRoutes/videoRoutes.js
+++ routes/clientRoutes/videoRoutes.js
@@ -24,7 +24,19 @@ const router = new Router();
  * Define routes start.
  */
 
-
+/**
+ * 视频详情
+ * GET /video/:videoId
+ * Request:
+ *      Param:
+ *          videoId 视频的_id
+ * Response:
+ *      Body:
+ *          {Video Object}
+ */
+router.get('video/:videoId', function *() {
+    this.body = yield MODEL.Video.findOne({"_id": this.params.videoId}).populate("doctor", "related");
+});
 
 /**
  * Define routes end.
