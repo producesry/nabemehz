ef4f45170164dcad76ab7274e92f2ba8286f7140
LiBook <libook7@qq.com>
2016-09-16 13:12:50 +0800

    Finish GET /doctor/:doctorId .

--- routes/clientRoutes/doctorRoutes.js
+++ routes/clientRoutes/doctorRoutes.js
@@ -24,7 +24,19 @@ const router = new Router();
  * Define routes start.
  */
 
-
+/**
+ * 医生详情页面
+ * GET /doctor/:doctorId
+ * Request:
+ *      Param:
+ *          doctorId 医生的_id
+ * Response:
+ *      Body:
+ *          {Doctor Object}
+ */
+router.get('doctor/:doctorId', function *() {
+    this.body = yield MODEL.Doctor.findOne({"_id": this.params.doctorId}).populate("related");
+});
 
 /**
  * Define routes end.
